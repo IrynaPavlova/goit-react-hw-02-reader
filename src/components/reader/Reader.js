f4981051b47ad47346683c972e7bb0edfa7a2b55
Pavlova <irochkapavlova85@gmail.com>
2020-01-10 15:18:14 +0200

    ready

--- src/components/reader/Reader.js
+++ src/components/reader/Reader.js
@@ -1,7 +1,5 @@
 import React from "react";
-import PropTypes from "prop-types";
 
 const Reader = ({ children }) => <div>{children}</div>;
 
-Reader.propTypes = {}; /////нужны ли??????
 export default Reader;
